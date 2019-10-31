0756c1ca539b5b427697b29873d81c40f520d859
あたらしまさとら <masatora.atarashi@gmail.com>
2019-11-01 03:44:03 +0900

    Finish Lesson2 tasks

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -19,13 +19,21 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+# print(len(labels_test))
 from sklearn.svm import SVC
-clf = SVC(kernel='rbf', gamma='1.0')
+clf = SVC(C=10000.0, kernel='rbf')
 clf.fit(features_train, labels_train)
 
 pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels_test, pred))
 
+import numpy as np
+print(np.size(np.where(pred == 1)))
 
 #########################################################
 ### your code goes here ###
